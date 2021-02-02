3481a79b7145aeb9bbabb7046e301e4342857632
Sharon <locky.lockandkey@gmail.com>
2021-02-02 12:23:51 +0800

    updated logout function

--- src/main/java/sg/edu/iss/app/AppConfig.java
+++ src/main/java/sg/edu/iss/app/AppConfig.java
@@ -1,8 +1,13 @@
 package sg.edu.iss.app;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
 @Configuration
 public class AppConfig {
-
+    @Bean
+    public SpringSecurityDialect springSecurityDialect(){
+        return new SpringSecurityDialect();
+    }
 }
