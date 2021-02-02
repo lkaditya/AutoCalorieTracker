49d04c4014defc970155513a4e4737e9b9a082b0
Sharon <locky.lockandkey@gmail.com>
2021-02-02 12:27:31 +0800

    updated logout function

--- src/main/java/sg/edu/iss/app/ThymeleafConfig.java
+++ src/main/java/sg/edu/iss/app/ThymeleafConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
 @Configuration
-public class AppConfig {
+public class ThymeleafConfig {
     @Bean
     public SpringSecurityDialect springSecurityDialect(){
         return new SpringSecurityDialect();
