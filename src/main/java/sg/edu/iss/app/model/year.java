983be07b4728c9d122cf06c1459f3ccd68d92a94
Sharon <locky.lockandkey@gmail.com>
2021-02-08 15:51:13 +0800

    register update, new year class to create Array from 1960 to 2021 for birthyear drop down list

--- src/main/java/sg/edu/iss/app/model/year.java
+++ src/main/java/sg/edu/iss/app/model/year.java
@@ -1,9 +1,13 @@
 package sg.edu.iss.app.model;
 
+import javax.persistence.Entity;
 import java.util.stream.IntStream;
 
+@Entity
 public class year {
 
-    int[] year = IntStream.range(1960, 2021).toArray();
+    public int year;
+
+    int[] number = IntStream.range(1960, 2021).toArray();
 
 }
