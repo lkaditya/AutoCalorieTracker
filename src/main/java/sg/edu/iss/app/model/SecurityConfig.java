b1c443a166e95cbdaae8efe5bf39696dd1b277dd
Sharon <locky.lockandkey@gmail.com>
2021-02-16 10:56:28 +0800

    SecurityConfig

--- src/main/java/sg/edu/iss/app/model/SecurityConfig.java
+++ src/main/java/sg/edu/iss/app/model/SecurityConfig.java
@@ -1,6 +1,8 @@
 package sg.edu.iss.app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -20,6 +22,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .logout();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
     }
 
 }
