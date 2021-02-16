ee8680ef087d016dc1fa98b6b517b090cf6ea272
Sharon <locky.lockandkey@gmail.com>
2021-02-16 21:59:43 +0800

    security config update

--- src/main/java/sg/edu/iss/app/model/SecurityConfig.java
+++ src/main/java/sg/edu/iss/app/model/SecurityConfig.java
@@ -1,12 +1,18 @@
 package sg.edu.iss.app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import javax.sql.DataSource;
 
@@ -20,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception{
         auth.jdbcAuthentication().dataSource(dataSource)
-        .usersByUsernameQuery("select email, password, enabled from user where email=?")
+        .usersByUsernameQuery("select email, {bcrypt}password, enabled from user where email=?")
         .authoritiesByUsernameQuery("select email, 'ROLE_USER' from user where email=?");
     }
 
