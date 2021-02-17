86b33371f778f71085d882abd9f7aea7367fd7d1
Sharon <locky.lockandkey@gmail.com>
2021-02-17 16:27:15 +0800

    SecurityConfig update

--- src/main/java/sg/edu/iss/app/SecurityConfig.java
+++ src/main/java/sg/edu/iss/app/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/login","/register", "/registerProcess", "/forgot_password").permitAll()
+                    .antMatchers("/", "/login","/register", "/registerProcess", "/forgot_password","/image/**").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
